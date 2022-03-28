81a7f0fa82d90a0aed34999bda9f925676e8ec2d
Gerald <me@jacobford.co.uk>
2022-03-28 02:02:31 +0100

    bytes test

---
+++ contracts/Bytes.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.8.0;
+
+contract Bytes {
+  function b(bytes memory) external {}
+}
---
+++ test/Bytes.js
@@ -0,0 +1,22 @@
+const { ethers } = require("hardhat");
+
+describe("Bytes", function () {
+  it("test", async () => {
+    const Bytes = await ethers.getContractFactory("Bytes");
+    const bytes = await Bytes.deploy();
+
+    for (let i = 0; i < 100; i++) {
+      const calldata =
+        "0x" +
+        Array((i + 1) * 1000)
+          .fill(0)
+          .map(() => "FF")
+          .join("");
+
+      console.log("calldata size:", calldata.length);
+      const tx = await bytes.b(calldata);
+      const resp = await tx.wait();
+      console.log("gas", resp.cumulativeGasUsed.toString());
+    }
+  });
+});
