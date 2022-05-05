a5e5e8c7387776db3b01509853f4c5371603bfc7
Gerald <gm@gerald.host>
2022-05-05 23:59:57 +0100

    fix

--- main/src/UToken.sol
+++ main/src/UToken.sol
@@ -67,36 +67,36 @@ contract UToken {
   /// @dev Staked tokens (DAI) is used to underwrite loan but is not the
   /// capital that is borrowed. Emits `LogStake` event
   /// @param amount Amount of token (DAI) to stake
-  function stake(uint128 amount) public {
-    Staker storage staker = stakers[msg.sender];
+  function stake(address who, uint128 amount) public {
+    Staker storage staker = stakers[who];
     staker.stakedAmount += amount;
 
-    _processStake(msg.sender, amount);
-    emit LogStake(msg.sender, amount);
+    _processStake(who, amount);
+    emit LogStake(who, amount);
   }
 
   /// @notice Unstake tokens (DAI) from contract
   /// @dev Can only unstake tokens that are not currently locked and underwriting
   /// active loans emits `LogUnstake` event
   /// @param amount Amount of token (DAI) to unstake
-  function unstake(uint128 amount) public {
-    Staker storage staker = stakers[msg.sender];
+  function unstake(address who, uint128 amount) public {
+    Staker storage staker = stakers[who];
     uint256 unstakeable = staker.stakedAmount - staker.outstanding;
     require(unstakeable >= amount, "!liquid");
     staker.stakedAmount -= amount;
 
-    _processUnstake(msg.sender, amount);
-    emit LogUnstake(msg.sender, amount);
+    _processUnstake(who, amount);
+    emit LogUnstake(who, amount);
   }
   
   /// @notice Borrow tokens (DAI) from contract
   /// @dev emits `LogBorrow` event
   /// @param amount Amount of tokens (DAI) to borrow
-  function borrow(uint128 amount) public {
+  function borrow(address who, uint128 amount) public {
     uint128 remaining = amount;
 
-    for(uint256 i = 0; i < vouchers[msg.sender].length; i++) {
-      Vouch storage vouch = vouchers[msg.sender][i];
+    for(uint256 i = 0; i < vouchers[who].length; i++) {
+      Vouch storage vouch = vouchers[who][i];
       
       uint128 borrowAmount = vouch.amount - vouch.outstanding;
       if(borrowAmount <= 0) continue;
@@ -111,15 +111,15 @@ contract UToken {
     }
 
     require(remaining <= 0, "!remaining");
-    _processBorrow(msg.sender, amount);
-    emit LogBorrow(msg.sender, amount);
+    _processBorrow(who, amount);
+    emit LogBorrow(who, amount);
   }
 
   /// @notice Update a vouch for a user
   function updateVouch(address staker, address borrower, uint128 amount) public {
     // TODO: remove staker argument
-    require(borrower != msg.sender, "!self vouch");
-    vouchers[msg.sender].push(Vouch(msg.sender, amount, 0));
+    require(borrower != staker, "!self vouch");
+    vouchers[borrower].push(Vouch(staker, amount, 0));
   }
 
   /* -------------------------------------------------------------------
@@ -127,8 +127,7 @@ contract UToken {
   ------------------------------------------------------------------- */
 
   /// @notice View function to get total vouch for a staker
-  function totalVouch() public view returns (uint256 total) {
-    address who = msg.sender;
+  function totalVouch(address who) public view returns (uint256 total) {
     for(uint256 i = 0; i < vouchers[who].length; i++) {
       Vouch memory vouch = vouchers[who][i];
       Staker memory staker = stakers[vouch.staker];
--- main/src/test/UToken.t.sol
+++ main/src/test/UToken.t.sol
@@ -9,27 +9,29 @@ contract UTokenTest is DSTest {
   
   UToken public utoken;
 
+  address bob = address(uint160(uint(keccak256(abi.encodePacked("suss")))));
+
   function setUp() external {
     utoken = new UToken();
-
-    utoken.stake(1000); 
     
     for (uint i = 0; i < 100; i++) {
       address voucher = address(uint160(uint(keccak256(abi.encodePacked(i + 100)))));
-      utoken.updateVouch(voucher, msg.sender, 100);
+      utoken.stake(voucher, 1000); 
+      utoken.updateVouch(voucher, bob, 10);
     }
   }
 
   function testBorrow500() external {
-    utoken.borrow(500);
+    emit log_uint(utoken.totalVouch(bob));
+    utoken.borrow(bob, 500);
   }
 
   function testBorrow1000() external {
-    utoken.borrow(1000);
+    utoken.borrow(bob, 1000);
   }
 
   function testUpdateTrust() external {
     address voucher = address(9999);
-    utoken.updateVouch(voucher, msg.sender, 100);
+    utoken.updateVouch(voucher, bob, 100);
   }
 }
